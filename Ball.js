83e4f002b17b00ad396a1f29ca065740c7fd0c2f
bijucksae <jiwonmin000@gmail.com>
2024-11-06 22:38:20 +0900

    Ball.js 추가

---
+++ Ball.js
@@ -0,0 +1,8 @@
+class Ball {
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.radius = 20;
+  }
+}
