7433d985c6438dab5cbe143a07b2799b9499ad0e
bijucksae <jiwonmin000@gmail.com>
2024-11-06 22:48:55 +0900

    중력 적용 및 공의 위치와 속도 업데이트

--- sketch.js
+++ sketch.js
@@ -4,3 +4,11 @@ function setup() {
   createCanvas(400, 400);
   gravity = creatveVector(0, 0.1);
 }
+
+function draw() {
+  background(220);
+
+  ball.applyForce(gravity);
+  ball.update();
+  ball.show(); 
+}
